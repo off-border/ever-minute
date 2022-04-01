8b2f0cf34c026c1edd3bdc178fefe2a5892875cb
off-border <git@alpah.ru>
2022-04-01 14:34:15 +0700

    master eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -71,6 +71,7 @@ module.exports = {
     'import/extensions': 'off',
     'import/no-unresolved': 'off',
     'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off',
 
     'prefer-promise-reject-errors': 'off',
 
